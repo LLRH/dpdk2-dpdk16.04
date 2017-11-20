ae8aefb5d01d4e5a8e0b629c34b7dd8f27a860c2
1016894282@qq.com <wenxingbeng>
2017-11-20 18:21:06 +0800

    fix bug

--- examples/l3fwd-send-register/main.c
+++ examples/l3fwd-send-register/main.c
@@ -841,7 +841,12 @@ main(int argc, char **argv)
 	ret = rte_eal_init(argc, argv);
 
     FILE *log = fopen("dpdk.log", "a+");
+    if(log == NULL){
+        printf("Cannot configure log\n");
+        return -1;
+    }
     rte_openlog_stream(log);
+
 
 	if (ret < 0)
 		rte_exit(EXIT_FAILURE, "Invalid EAL parameters\n");
