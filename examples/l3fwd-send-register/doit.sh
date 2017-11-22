db5b08e7856f8eec4f13118fa7c499c7d51311d3
1016894282@qq.com <wenxingbeng>
2017-11-22 12:00:14 +0800

    变量忘记加美元符号了

--- examples/l3fwd-send-register/doit.sh
+++ examples/l3fwd-send-register/doit.sh
@@ -8,7 +8,7 @@
  fi
  sleep 1s
 
- ./build/l3fwd -c 0x7 -n 2 --log-level logLevel -- -P -E -p 0x3 --config="(0,0,1),(1,0,2)"
+ ./build/l3fwd -c 0x7 -n 2 --log-level $logLevel -- -P -E -p 0x3 --config="(0,0,1),(1,0,2)"
 
 
 #./build/l3fwd -c 0xF3E0 -n 3  --proc-type auto \
